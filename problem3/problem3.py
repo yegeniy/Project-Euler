fec4556dbf0a7c01cb7e55af61e2ed1904dd5540
yegeniy <evulfson@gmail.com>
2012-01-08 01:20:04 -0500

    tests mostly pass. Correct answer under 1 min [P3]
    This fails:
      self.assertEqual(5, self.problem3.largestPrimeFactor(5))
    
    even though
    
    > python problem3/problem3.py 600851475143
    
    gives the correct answer

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -1,30 +1,52 @@
 import sys
+import math
 
 class ProblemThree:
 
+  def __init__(self):
+    self.knownPrimes = [2]
+
   def largestPrimeFactor(self, n):
+    maximum = n #int(math.floor(math.sqrt(n)))
+    # print maximum
+    for divisor in self.findFactors(n, 2, maximum+1):
+      dividend = n/divisor
+      print "divisor:", divisor,"; dividend:", dividend
+      # print "largestPrimeFactor(",n,"). Dividend: ", dividend
+      if self.isPrime(dividend):
+        return dividend
 
-    for factor in self.findFactors(n):
-      if factor == 1:
-        return n
-      if self.isPrime(factor):
-        return factor
+  def isPrime(self, n):
+    for p in self.calcPrimes(int(math.floor(math.sqrt(n)))):
+      # print "isPrime(",n,"): ", p
+      if p>=n:
+        return True
+      if n % p == 0:
+        return False
+    return True
 
-  def findFactors(self,n):
-    for x in range(n/2, 1, -1):
-      while n%x != 0 and x > 1:
-        x -= 1
-      yield x
+  def calcPrimes(self, ceiling):
+    # returns a list of known primes up to and including ceiling
+    for p in self.knownPrimes:
+      yield p
+    for x in xrange(self.knownPrimes[len(self.knownPrimes)-1]+1, ceiling+1):
+      isPrime = True
+      for p in self.knownPrimes:
+        # print p, x
+        # TODO: List Comprehension
+        # if p < int(math.floor(math.sqrt(x))):
+          if x % p == 0:
+            # print x, " divisible by ", p
+            isPrime = False
+            break
+      if isPrime:
+        self.knownPrimes.append(x)
+        yield x
 
-  def findFactor(self, n):
-    divisor = n/2
-    while n%divisor != 0 and divisor > 1:
-      divisor -= 1
-    return divisor
-
-  def isPrime(self, factor):
-    return self.findFactors(factor).next() == 1
-
+  def findFactors(self, n, start, end):
+    for x in xrange(start, end):
+      if n%x == 0:
+        yield x
 
 if __name__ == '__main__':
   problem3 = ProblemThree()
--- problem3/test_problem3.py
+++ problem3/test_problem3.py
@@ -3,10 +3,26 @@ from problem3 import ProblemThree
 
 class TestProblem(unittest.TestCase):
 
-  def test_(self):
-    problem3 = ProblemThree()
-    self.assertEqual(29, problem3.largestPrimeFactor(13195))
-    self.assertEqual(5, problem3.largestPrimeFactor(5))
+  def setUp(self):
+    self.problem3 = ProblemThree()
+
+  def test_largestPrimeFactor(self):
+    self.assertEqual(29, self.problem3.largestPrimeFactor(13195))
+    self.assertEqual(5, self.problem3.largestPrimeFactor(5))
+
+  def test_isPrime(self):
+    self.assertFalse(self.problem3.isPrime(4))
+    self.assertTrue(self.problem3.isPrime(29))
+    self.assertTrue(self.problem3.isPrime(5))
+    self.assertTrue(self.problem3.isPrime(2))
+
+
+  def test_calcPrimes(self):
+    self.assertEqual([2], list(self.problem3.calcPrimes(2)))
+    self.assertEqual([2,3], list(self.problem3.calcPrimes(3)))
+    self.assertEqual([2,3,5], list(self.problem3.calcPrimes(6)))
+    self.assertEqual([2,3,5,7,11,13,17,19,23,29], list(self.problem3.calcPrimes(29)))
+    # print len(list(self.problem3.calcPrimes(775146)))
 
 if __name__ == '__main__':
     unittest.main()
